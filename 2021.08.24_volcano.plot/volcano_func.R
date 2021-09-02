82d58ec3dfda8062d02f4b9e74b64c351568ab4d
Kimberly Dill-McFarland <21342185+kdillmcfarland@users.noreply.github.com>
2021-09-01 17:52:47 -0700

    init code review, comments only

--- 2021.08.24_volcano.plot/volcano_func.R
+++ 2021.08.24_volcano.plot/volcano_func.R
@@ -19,13 +19,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Input parameters:
 REQUIRED
     file_name = Thsi is the file generated from extract_pval function
-
+#Input should be a data frame, not file
+#For consistency with kimma, suggest df name is 'fdr'
 
 OPTIONAL
   coeff = By default the function will print all coefficients from the linear model. can also specify specific coefficients e.g. c('coeff1', 'coeff2')
   fdr = You can set the FDR correction by default it is 0.2
   n_top_gene = number of up regulated and down regulated genes to label with hgnc symbol and color. default is 10
   mod_gene = you can specify if its modules or genes in you data for the plot. Defaut is gene
+
+#Suggest parameters like
+## col_cutoff = fdr cutoff for colors
+## label_n = to replace n_top_gene
+## label_cutoff = fdr cutoff to label genes (error message if both col_cutoff and label_cutoff are set)
 
 Example
   volcano_funcxn(file_name='results/test.pval_w_hgnc.csv',
@@ -35,6 +41,8 @@ Example
 "
 # All the libraries
 volcano_funcxn <- function(file_name, coeff = "all",fdr = 0.2, n_top_gene = 10,mod_gene = "gene", out_dir = "./") {
+#Packages do not need to be installed for a function in a custom package
+#Similarly in a package, library() is not used. Instead, designate each non-base R function to a given package such as dplyr::select()
   for (package in c('tidyverse', 'ggplot2', 'ggrepel')) {
     if (!require(package, character.only=T, quietly=T)) {
       install.packages(package)
@@ -53,34 +61,39 @@ volcano_funcxn <- function(file_name, coeff = "all",fdr = 0.2, n_top_gene = 10,m
   }
   else {
   }
+    #A lot of code if repeated in this ifelse section
+    #Suggest instead using input parameters to set custom column names within the fdr df. Defaults should be those from kimma::fmFit output
   if(mod_gene == "gene"){
     # running for loop for each given coefficient
   for (i in coeff) {
     pval_coef <- subset(p_val, group == i)
     # getting all hgnc symbol for up regulated genes
-    up_reg <-  head(subset(pval_coef, FC.group == "up" & adj.P.Val < fdr) %>% arrange(adj.P.Val), n_top_gene) %>%
-              rename(delabel = hgnc_symbol) %>%
+    up_reg <-  head(subset(pval_coef, FC.group == "up" & adj.P.Val < fdr) %>% arrange(adj.P.Val), n_top_gene) %>% #FC.group may not be present in other user's data. Suggest using the estimate (logFC) to define up/down from pos/neg
+              rename(delabel = hgnc_symbol) %>% #Suggest parameter to set what column to pull label info from
               select(geneName, delabel)
     # getting all hgnc symbol for down regulated genes
     down_reg <-  head(subset(pval_coef, FC.group == "down" & adj.P.Val < fdr) %>% arrange(adj.P.Val), n_top_gene) %>%
                 rename(delabel = hgnc_symbol) %>%
                 select(geneName, delabel)
     # making the dataframe to plot
-    pval_plot <- left_join(pval_coef,down_reg, by="geneName") %>%
+    pval_plot <- left_join(pval_coef,down_reg, by="geneName") %>% #mutate(ifelse) is a shorter way to add a delabel column to the df
                 left_join(.,up_reg,by="geneName") %>%
                 mutate(delabel = coalesce(delabel.x,delabel.y)) %>%
                 mutate(FC.group = case_when(is.na(delabel) == FALSE ~ FC.group,
-                                  is.na(delabel) == TRUE ~ "No"))
-    cols <- c("up"="red","down"="blue","No"="black")
+                                  is.na(delabel) == TRUE ~ "No")) #No is a confusing label for FC group. These have a fold change, they just aren't colored
+    cols <- c("up"="red","down"="blue","No"="black") #Suggest parameter to custom set colors if desired. Defaults maybe should be a little less harsh than these - diffcult to read the text labels
     # running ggplot to make volcano plot
-    plt_vol<-ggplot(data=pval_plot, aes(x=logFC, y=-log10(adj.P.Val), col=FC.group, label=delabel)) +
+    plt_vol<-ggplot(data=pval_plot, aes(x=logFC, y=-log10(adj.P.Val), col=FC.group, label=delabel)) + 
+      #Suggest creating a base ggplot without all aes set then using if statements to add layers like color and labeling only if 
+      #they are given as input parameters. This allows the user to use color, label, both, or neither
       geom_point() +
       theme_minimal() +
       geom_text_repel() +
       scale_color_manual(values=cols) +
       geom_vline(xintercept=0, col="black") +
       geom_hline(yintercept= 0, col="black")
-    ggsave(paste(out_dir,i,"_volcano_plot.pdf"))
+      #Suggest vline at fdr cutoff if used
+    ggsave(paste(out_dir,i,"_volcano_plot.pdf")) #Save plots to list object within R instead
   }
   }
     else {
